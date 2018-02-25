4492b881407ab98556625ffb6c57d91df5a41bf8
LincolnTunapanda <lincolnajanga@gmail.com>
2018-02-25 09:03:39 +0300

    Jquery logic

--- main.js
+++ main.js
@@ -1,3 +1,62 @@
-// Logic behind the ping pong game
-  var pickNum = prompt(" ");
+// Backend Logic for the ping pong game
+
+//   var pingGenerated = function(userInput) {
+// 	var arrayResult = [];
+// 	//for loop
+// 	for(var i = 1; i <= userInput; i ++ ) {
+// 		if(((i % 3) === 0) && ((i % 5) === 0)) {
+// 			arrayResult.push("pingpong");
+// 		} else if ((i % 3) === 0) {
+// 			arrayResult.push("ping");
+// 		} else if ((i % 5) === 0) {
+// 			arrayResult.push("pong");
+// 		} else {
+// 			arrayResult.push(i);
+// 		}
+// 	}
+// 	return arrayResult;
+// }
+
+// console.log(pingGenerated);
+
+
+
+
+
+var pingPong = function pingPongGenerator(userInput){
+
+var arrayResult = [];
+
+for (var i = 1; i <= userInput; i++){
+ if(i % 3===0)
+ 	arrayResult.push('ping');
+ }
+ else if (i % 5===0){
+ 	arrayResult.push('pong');
+ }
+ else if(i % 3===0) && (i % 5===0){
+ 	arrayResult.push('ping pong');
+ }
+ else{
+ 	arrayResult.push(i);
+ }
+}
+
+return arrayResult;
+}
+
+//User interface logic
+
+//jquery 
+$(document).ready(function() {
+	$('#onClick').click(function(event) {
+		event.preventDefault();
+
+		//get user input
+		var userInput = parseInt($("").val());
+        var result = pingPong(userInput);
+	})
+	
+})
+
 
